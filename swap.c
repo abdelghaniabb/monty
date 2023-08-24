571ef686045496b658876f59df758753f46c123f
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-08-24 19:40:50 +0100

    task3

--- swap.c
+++ swap.c
@@ -7,17 +7,15 @@ void swap(stack_t **stack, unsigned int line_number)
 	while ((*stack)->prev != NULL)
 		*stack = (*stack)->prev;
 
-	if ((*stack == NULL) || ((*stack)->next = NULL))
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short", line_number);
 		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
-printf("fh\n");
+
 	tmp = (*stack)->n;
-	
-	(*stack)->n = ((*stack)->next)->n;
-	printf("fh1\n");
-	((*stack)->next)->n = tmp;
-	
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = tmp;
 }
+
